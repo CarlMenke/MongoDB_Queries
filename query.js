6d3f15837eca5aaad5505fefc57ac4cb8e413e1c
carlmenke <carlmenke13@gmail.com>
2022-09-06 16:46:25 -0700

    added find bopok function in query.js

--- query.js
+++ query.js
@@ -3,11 +3,24 @@ const { Book } = require('./models')
 
 async function main() {
   try {
+     await findBook()
   } catch (error) {
     console.log(error)
   } finally {
     await db.close()
-  }
+}
 }
 
+
+async function findBook() {
+
+    const book = await Book.find({title:"The Power of Now"})
+    console.log('book', book)
+
+
+    // console.log(await Book.find({title:"The Power of Now"}))
+
+
+}
 main()
+
